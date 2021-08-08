793e49fd8e8ebbd6723c1901f2b5bf574fefea59
Halil <halil.sahin@solviads.com>
2021-08-08 09:56:45 +0300

    initial commit

---
+++ src/main/java/com/halilsahin/weatherapp/controller/WeatherController.java
@@ -0,0 +1,7 @@
+package com.halilsahin.weatherapp.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WeatherController {
+}
---
+++ src/main/java/com/halilsahin/weatherapp/route/SimpleRouteBuilder.java
@@ -0,0 +1,11 @@
+package com.halilsahin.weatherapp.route;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class SimpleRouteBuilder extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+       // from("file:").to("file:");
+
+    }
+}
